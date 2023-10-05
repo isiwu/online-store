5404a359d0e45595b44d0c062a28c8c67cdf073d
Emmanuel <isiwuemma@gmail.com>
2023-10-05 22:50:43 +0100

    product update feature added

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -46,4 +46,31 @@ export class AdminProductsController {
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
+
+  @Get('/:id')
+  @Render('admin/products/edit')
+  async edit(@Param('id') id: string) {
+    const viewData = [];
+    viewData['title'] = 'Admin Page - Edit Product - Online Store';
+    viewData['product'] = await this.productsService.findOne(id);
+
+    return {
+      viewData,
+    };
+  }
+
+  @Post('/:id/update')
+  @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
+  @Redirect('/admin/products')
+  async update(
+    @Body() body,
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') id: string,
+  ) {
+    const product = await this.productsService.findOne(id);
+    product.setName(body.name);
+    product.setPrice(body.price);
+    file && product.setImage(file.filename);
+    await this.productsService.createOrUpdate(product);
+  }
 }
