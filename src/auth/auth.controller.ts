81183742926121d60f393f88ec659ef5db002bcd
Emmanuel <isiwuemma@gmail.com>
2023-10-06 23:45:46 +0100

    authetication controller updated for login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, Redirect, Render } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Redirect,
+  Render,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { User } from 'src/models/user.entity';
 import { UsersService } from 'src/models/users.service';
 
@@ -28,5 +37,41 @@ export class AuthController {
     user.setRole('client');
     user.setBalance(1000);
     await this.usersService.createOrUpdate(user);
+  }
+
+  @Get('/login')
+  @Render('auth/login')
+  login() {
+    const viewData = [];
+    viewData['title'] = 'User Login - Online Store';
+    viewData['subtitle'] = 'User Login';
+
+    return {
+      viewData,
+    };
+  }
+
+  @Post('/connect')
+  async connect(@Body() body, @Req() req, @Res() res) {
+    const { email, password } = body;
+    const user = await this.usersService.login(email, password);
+
+    if (user) {
+      req.session.user = {
+        id: user.getId(),
+        name: user.getName(),
+        role: user.getRole(),
+      };
+
+      return res.redirect('/');
+    } else {
+      return res.redirect('/auth/login');
+    }
+  }
+
+  @Get('/logout')
+  @Redirect('/')
+  logout(@Req() req) {
+    req.session.user = null;
   }
 }
